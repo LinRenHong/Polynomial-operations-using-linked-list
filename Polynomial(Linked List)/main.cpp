1b892bf5aa9aa11865bf3431e37d6c51f5ec31df
RenHong <appleken51688@gmail.com>
2019-05-14 00:51:49 +0800

    Initial Commit

---
+++ Polynomial(Linked List)/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Polynomial(Linked List)
+//
+//  Created by 林仁鴻 on 2019/5/14.
+//  Copyright © 2019 林仁鴻. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
